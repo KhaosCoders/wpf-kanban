80cdb15eea7c72c92962b22c9a98285de532a4db
Tatenshi <joshi.franz@gmail.com>
2021-07-28 09:24:04 +0200

    New SDK Style

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -4,18 +4,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("WPF-Kanban")]
-[assembly: AssemblyDescription("A simple task board WPF control")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("KhaosCoders")]
-[assembly: AssemblyProduct("WPF-Kanban")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -38,16 +26,3 @@ using System.Windows;
                                       //(used if a resource is not found in the page,
                                       // app, or any theme specific resource dictionaries)
 )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.0")]
-[assembly: AssemblyFileVersion("1.5.1.0")]
