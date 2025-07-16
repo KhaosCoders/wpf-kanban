67c98c45fbdf21db66c1931774df1b23a21f8876
Khaos <khaos@khaos-coders.org>
2025-07-16 21:40:20 +0200

    Added UseBlockerColoring property

---
+++ Source/Converter/MultiBoolConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace KC.WPF_Kanban.Converter;
+
+public class MultiBoolConverter : IMultiValueConverter
+{
+    public enum MultiBoolCondition
+    {
+        Any, All, None
+    }
+
+    public MultiBoolCondition Condition { get; set; } = MultiBoolCondition.All;
+
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool visible = false;
+        switch (Condition)
+        {
+            case MultiBoolCondition.Any:
+                visible = values.Any(o => o is bool b && b);
+                break;
+            case MultiBoolCondition.All:
+                visible = values.All(o => o is bool b && b);
+                break;
+            case MultiBoolCondition.None:
+                visible = !values.Any(o => o is bool b && b);
+                break;
+        }
+        return visible;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
